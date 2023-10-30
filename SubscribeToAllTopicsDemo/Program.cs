e578a0b2277a973fb409604c1798271b37b4719f
Oliver Abraham <mail@oliver-abraham.de>
2023-10-30 17:10:24 +0100

    SubscribeToAllTopics

---
+++ SubscribeToAllTopicsDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Abraham.MQTTClient;
+
+namespace SubscribeToAllTopicsDemo;
+
+internal class Program
+{
+    private static string _url      = "<YOUR MQTT BROKER URL WITHOUT PROTOCOL WITHOUT PORT>";
+    private static string _username = "<YOUR MQTT BROKER USERNAME>";
+    private static string _password = "<YOUR MQTT BROKER PASSWORD>";
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Connecting to MQTT broker and subscribing to a topic (listening to value changes)");
+
+        #if DEBUG
+        _url      = File.ReadAllText(@"C:\Credentials\mosquitto_url.txt");
+        _username = File.ReadAllText(@"C:\Credentials\mosquitto_username.txt");
+        _password = File.ReadAllText(@"C:\Credentials\mosquitto_password.txt");
+        #endif
+
+        var client = new MQTTClient()
+            .UseUrl(_url)
+            .UseUsername(_username)
+            .UsePassword(_password)
+            .Build();
+
+        client.SubscribeToAllTopics();
+        Console.WriteLine("Now listening to all topics...  press any key to end the program.");
+
+        client.OnEvent = 
+            delegate(string topic, string value)
+            {
+                Console.WriteLine($"New event received: {topic}={value}");
+            };
+
+        Console.ReadKey();
+        client.StopAllSubscriptions();
+    }
+}
